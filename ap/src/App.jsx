c57cb7a476e8be2b7b6852a600e24dcfc4f2953c
sauravumla <148962900+sauravumla@users.noreply.github.com>
2023-12-25 08:49:39 +0530

    add ap navbar to ap

--- ap/src/App.jsx
+++ ap/src/App.jsx
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
+
     <div className="App">
-      
+      <Navbar></Navbar>
+      <h1>hello</h1>
     </div>
   );
 }
