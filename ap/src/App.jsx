da624b3b75f1a1d69a0d974c3f3db3b1c8a47a2d
sauravs4h <saurav@umla.in>
2023-12-28 06:20:36 +0530

    aa

--- ap/src/App.jsx
+++ ap/src/App.jsx
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 function App() {
   return (
 
     <div className="App">
       <Navbar></Navbar>
+      <Hero></Hero>
       <h1>hello</h1>
     </div>
   );
